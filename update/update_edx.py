9e0b033eb084ee2b8a56305c1db84a35366e8a7e
wowdd1 <developergf@gmail.com>
2014-12-09 21:35:28 +0800

    get all subjects data for coursera edx and harvard

--- update/update_edx.py
+++ update/update_edx.py
@@ -7,8 +7,75 @@
 from common import *
 
 dir_name = "edx/"
+edx_subject = [\
+"Architecture",
+"Art & Culture",
+"Biology & Life Sciences",
+"Business & Management",
+"Chemistry",
+"Communication",
+"Computer Science",
+"Design",
+"Economics & Finance",
+"Education",
+"Electronics",
+"Energy & Earth Sciences",
+"Engineering",
+"Environmental Studies",
+"Ethics",
+"Food & Nutrition",
+"Health & Safety",
+"History",
+"Humanities",
+"Law",
+"Literature",
+"Math",
+"Medicine",
+"Music",
+"Philanthropy",
+"Philosophy & Ethics",
+"Physics",
+"Science",
+"Social Sciences",
+"Statistics & Data Analysis"]
 
 #edx
+
+def match_subject(subject, subjects):
+    for sub in subjects:
+        if sub != "" and sub.strip() == subject:
+            return True
+
+    return False
+
+
+def getEdxOnlineCourse(subject, json_obj):
+    for obj in json_obj:
+        if match_subject(subject, obj["subjects"]):
+            file_name = get_file_name(dir_name + subject)
+            file_lines = countFileLineNum(file_name)
+            f = open_db(file_name + ".tmp")
+            count = 0
+
+            print "processing json and write data to file..."
+            for item in json_obj:
+                title = item["l"].strip() + " (" + item["schools"][0].strip() + ")"
+                title = delZh(title)
+                count = count + 1
+                write_db(f, item["code"].strip() + " " + title)
+                write_db_url(url_f, item["code"].strip(), item["url"], title)
+
+            close_db(f)
+            if file_lines != count and count > 0:
+                do_upgrade_db(file_name)
+                print "before lines: " + str(file_lines) + " after update: " + str(count) + " \n\n"
+            else:
+                cancel_upgrade(file_name)
+                print "no need upgrade\n" 
+    
+
+
+
 print "downloading edx course info"
 r = requests.get("https://www.edx.org/search/api/all")
 
@@ -18,29 +85,12 @@ jobj = json.loads(r.text)
 
 truncateUrlData(dir_name)
 url_f = open_url_file(dir_name)
-file_name = get_file_name(dir_name + "eecs")
-file_lines = countFileLineNum(file_name)
-f = open_db(file_name + ".tmp")
-count = 0
 
-print "processing json and write data to file..."
-for item in jobj:
-    for subject in item["subjects"]:
-        if subject == "Computer Science" or subject == "Electronics" or subject == "Statistics & Data Analysis":
-            title = item["l"].strip() + " (" + item["schools"][0].strip() + ")"
-            title = delZh(title)
-            count = count + 1
-            write_db(f, item["code"].strip() + " " + title)
-            write_db_url(url_f, item["code"].strip(), item["url"], title)
 
-close_db(f)
+for subject in edx_subject:
+    if subject != "":
+        getEdxOnlineCourse(subject, jobj)
+
 close_url_file(url_f)
-if file_lines != count and count > 0:
-    do_upgrade_db(file_name)
-    print "before lines: " + str(file_lines) + " after update: " + str(count) + " \n\n"
-else:
-    cancel_upgrade(file_name)
-    print "no need upgrade\n"
-
 
 
--- update/update_harvard.py
+++ update/update_harvard.py
@@ -7,56 +7,67 @@
 from common import *
 
 dir_name = "harvard/"
-
+root_url = "http://www.registrar.fas.harvard.edu"
 
 #harvard
-#"""
+
+def getHarvardCourse(subject, url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text)
+    sys.setrecursionlimit(3000)
+    file_name = get_file_name(dir_name + subject)
+    file_lines = countFileLineNum(file_name)
+    f = open_db(file_name + ".tmp")
+    count = 0
+
+
+    print "\n\nprocessing " + subject + " html and write data to file..."
+    for node in soup.find_all("strong"):
+        text = ""
+        link = ""
+        if node.string == None:
+            if node.a != None and node.a.string != None:
+                text = node.a.string.replace("\n", "")
+                link = node.a["href"]
+            else:
+                if node.a != None:
+                    link = node.a["href"]
+                text = node.prettify()
+                if text.find("href=") > 0 :
+                    text = text[text.find(">", 8) + 1 : text.find("<", text.find(">", 8)) - 1]
+                else:
+                    text = text[text.find(">", 2) + 1 : text.find("<", 8) - 1]
+                text = text.replace("\n", "").strip()
+        else:
+            text = node.string.replace("\n", "")
+
+        count = count + 1
+        #print text
+        write_db(f, text)
+        write_db_url(url_f, text[0:text.find(".")], link, text[text.find(".") + 2:])
+    if count == 0:
+        print subject + " can not get the data, check the html and python code"
+    
+    close_db(f)
+    if file_lines != count and count > 0:
+        do_upgrade_db(file_name)
+        print "before lines: " + str(file_lines) + " after update: " + str(count) + " \n\n"
+    else:
+        cancel_upgrade(file_name)
+        print "no need upgrade\n"
+    
+
 print "downloading harvard course info"
-r = requests.get("http://www.registrar.fas.harvard.edu/courses-exams/courses-instruction/computer-science")
+r = requests.get("http://www.registrar.fas.harvard.edu/courses-exams/courses-instruction")
 soup = BeautifulSoup(r.text)
-sys.setrecursionlimit(1400)
 
 truncateUrlData(dir_name)
 url_f = open_url_file(dir_name)
-file_name = get_file_name(dir_name + "cs")
-file_lines = countFileLineNum(file_name)
-f = open_db(file_name + ".tmp")
-count = 0
 
 
-print "processing html and write data to file..."
-for node in soup.find_all("strong"):
-    text = ""
-    link = ""
-    if node.string == None:
-        if node.a != None and node.a.string != None:
-            text = node.a.string.replace("\n", "")
-            link = node.a["href"]
-        else:
-            if node.a != None:
-                link = node.a["href"]
-            text = node.prettify()
-            if text.find("href=") > 0 :
-                text = text[text.find(">", 8) + 1 : text.find("<", text.find(">", 8)) - 1]
-            else:
-                text = text[text.find(">", 2) + 1 : text.find("<", 8) - 1]
-            text = text.replace("\n", "").strip()
-    else:
-        text = node.string.replace("\n", "")
-
-    count = count + 1
-    write_db(f, text)
-    write_db_url(url_f, text[0:text.find(".")], link, text[text.find(".") + 2:])
+for span in soup.find_all("span", class_="field-content"):
+    #print span.a.string
+    getHarvardCourse(span.a.string, root_url + str(span.a["href"]))
 
 
-
-close_db(f)
 close_url_file(url_f)
-if file_lines != count and count > 0:
-    do_upgrade_db(file_name)
-    print "before lines: " + str(file_lines) + " after update: " + str(count) + " \n\n"
-else:
-    cancel_upgrade(file_name)
-    print "no need upgrade\n"
-#"""
-
