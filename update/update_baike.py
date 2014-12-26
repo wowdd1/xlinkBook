6e749b4c2d0f7eee8c71b2240f6c76efef6cba6a
wowdd1 <developergf@gmail.com>
2014-12-26 11:44:12 +0800

    add turing-award and computer-pioneer-award info

--- update/update_baike.py
+++ update/update_baike.py
@@ -42,7 +42,86 @@ class BaikeSpider(Spider):
             self.cancel_upgrade(file_name)
             print "no need upgrade\n"
 
+    def processWikiTuringData(self, url):
+
+        file_name = self.get_file_name("eecs/Turing-Award", self.school)
+        file_lines = self.countFileLineNum(file_name)
+        f = self.open_db(file_name + ".tmp")
+        self.count = 0
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text);
+        for tr in soup.find_all("tr"):
+            if tr.th != None and tr.text.strip()[0:1] == "1" or tr.text.strip()[0:1] == "2":
+                i = 0
+                link = "http://en.wikipedia.org" + str(tr.td.a["href"])
+                year = ""
+                title = ""
+                remark = ""
+                for line in tr.text.strip().split("\n"):
+                    i += 1
+                    if i == 1:
+                        year = line
+                        continue
+                    if i == 2 or len(line) < 50:
+                        title += " " + line
+                        continue
+                    if i == 3:
+                        print year + " " + title + " " + link
+                        remark = line
+                        self.count += 1
+                        self.write_db(f, year, title.strip(), link, remark)
+                        i = 0
+                        continue
+
+        self.close_db(f)
+        if file_lines != self.count and self.count > 0:
+            self.do_upgrade_db(file_name)
+            print "before lines: " + str(file_lines) + " after update: " + str(self.count) + " \n\n"
+        else:
+            self.cancel_upgrade(file_name)
+            print "no need upgrade\n"
+
+    def processWikiPioneerData(self, url):
+
+        file_name = self.get_file_name("eecs/Computer-Pioneer-Award", self.school)
+        file_lines = self.countFileLineNum(file_name)
+        f = self.open_db(file_name + ".tmp")
+        self.count = 0
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text);
+        title = ""
+        for tr in soup.find_all("tr"):
+            #print tr.text.strip()         
+            if tr.text.strip()[0:1] == "1" or tr.text.strip()[0:1] == "2":
+                title = title.strip()
+                if len(title) > 0 and title[0:1] == "1" or title[0:1] == "2":
+                    self.write_db(f, title[0: title.find(" ")], title[title.find(" ") + 1 :], "")
+                    print title
+                    self.count += 1
+                title = ""
+                for line in tr.text.strip().split("\n"):
+                    title += " " + line.strip()
+
+            else:
+                title += " " + tr.text.replace("\n", " ").strip()
+
+        title = title.strip()
+        if len(title) > 0 and title[0:1] == "1" or title[0:1] == "2":
+            self.write_db(f, title[0: title.find(" ")], title[title.find(" ") + 1 :], "")
+            print title
+            self.count += 1
+                
+        self.close_db(f)
+        if file_lines != self.count and self.count > 0:
+            self.do_upgrade_db(file_name)
+            print "before lines: " + str(file_lines) + " after update: " + str(self.count) + " \n\n"
+        else:
+            self.cancel_upgrade(file_name)
+            print "no need upgrade\n"
+
     def do_work(self):
+        self.processWikiTuringData("http://en.wikipedia.org/wiki/Turing_Award")
+        self.processWikiPioneerData("http://en.wikipedia.org/wiki/Computer_Pioneer_Award")
 
         self.processBaikeData("http://www.baike.com/wiki/IT%E4%B8%9A%E6%9C%80%E5%85%B7%E5%BD%B1%E5%93%8D%E5%8A%9B%E7%9A%84284%E4%BD%8D%E7%A8%8B%E5%BA%8F%E5%91%98")
 
