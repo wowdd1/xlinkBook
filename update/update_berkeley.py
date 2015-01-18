71a39339b6cdba2d5b262fce0f6abd50d9490f5a
wowdd1 <developergf@gmail.com>
2015-01-18 18:43:40 +0800

    BeautifulSoup not work for berkeley, replace it with htmlparse module, and get the real url for course

--- update/update_berkeley.py
+++ update/update_berkeley.py
@@ -6,6 +6,55 @@
     
 from spider import *
 
+py3k = sys.version_info[0] >= 3
+if py3k:
+    from html.parser import HTMLParser
+else:
+    from HTMLParser import HTMLParser
+
+class TableHandler(HTMLParser):
+    def __init__(self, **kwargs):
+        HTMLParser.__init__(self)
+        self.kwargs = kwargs
+        self.active = None
+        self.last_content = ""
+        self.rows = []
+        self.found_first_valid_num = False
+
+    def handle_starttag(self,tag, attrs):
+        self.active = tag
+
+    def handle_endtag(self,tag):
+        #if tag in ["th", "td"]:
+        #    print self.last_content.strip()
+        #if tag in ['tr']:
+        #    self.last_content = " "
+
+        self.active = None
+    def valid_course_num(self, text):
+        if text.find(' ') == -1 and text != '&' and text != 'unknown' and text != 'Neural' and text != 'Mechatronics' and text != 'Cryptography':
+            return True
+        return False
+
+    def handle_data(self, data):
+        data = data.replace('\n', '').strip()
+        if len(data) == 0 or data == '[' or data == ']' or data == 'archives':
+            return
+
+        if self.valid_course_num(data):
+            if self.found_first_valid_num:
+                self.rows.append(self.last_content)
+               
+            if data[0 : 2] == 'CS': 
+                self.found_first_valid_num = True
+            self.last_content = ""
+ 
+        #print data.strip()
+        if data[0: 4] == 'Last':
+            return
+        self.last_content += data + ' '
+
+
 class BerkeleySpider(Spider):
     
 
@@ -13,7 +62,15 @@ class BerkeleySpider(Spider):
         Spider.__init__(self)
         self.school = "berkeley"
         self.subject = "eecs"
-    
+   
+    def getLink(self, url):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text)
+        links = []
+        for a in soup.find_all('a'):
+            if a['href'][0 : 2] == './':
+                links.append(url.replace('archives.html', '') + a['href'][2:])
+        return links[len(links) - 1]
     
     def processBerkeleyData(self, f, tr):
         if self.need_update_subject(self.subject) == False:
@@ -27,24 +84,58 @@ class BerkeleySpider(Spider):
            if i == 5:
                title = title + td.u.string
                link = "http://www-inst.eecs.berkeley.edu" + td.a["href"]
+               link = self.getLink(link)
            i = i + 1
         if i > 4:
             self.count = self.count + 1
             self.write_db(f, title[0:title.find(" ")], title[title.find(" "):], link)
-    
+
+    def genUrl(self, course_num):
+        return 'http://www-inst.eecs.berkeley.edu/~' + course_num.lower() + '/archives.html'    
+
+    def getRealUrl(self, url):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text)
+        link_dict = {}
+        for a in soup.find_all('a'):
+            if a.attrs.get('href', '') != '' and a['href'][0 : 2] == './':
+                key = a.text.strip().replace('Spring ', '').replace('Summer ', '').replace('Fall ', '').replace('Winter ', '')
+                link_dict[key] = (url.replace('archives.html', '') + a['href'][2:])
+        sorted_keys = sorted(link_dict)
+
+        for i in range(len(sorted_keys) - 1, -1 , -1):
+            test_url = link_dict[sorted_keys[i]]
+            r = requests.get(test_url)
+            if r.status_code == 200:
+                soup = BeautifulSoup(r.text)
+                for a in soup.find_all('a'):
+                    if a.text.strip() == url:
+                        #print test_url + ' not match'
+                        break
+                #print 'match ' + test_url
+                return test_url
+        if len(sorted_keys) > 0:
+            return link_dict[sorted_keys[len(sorted_keys) - 1]]
+        else:
+            return url
+
     def doWork(self):
         #berkeley
         #"""
         print "downloading berkeley course info"
         r = requests.get("http://www-inst.eecs.berkeley.edu/classes-cs.html")
-        soup = BeautifulSoup(r.text)
-    
+
+        parser = TableHandler()
+        parser.feed(r.text)
+
         file_name = self.get_file_name(self.subject, self.school)
         file_lines = self.countFileLineNum(file_name)
         f = self.open_db(file_name + ".tmp")
+        self.count = 0
     
         print "processing html and write data to file..."
-        for table in soup.find_all("table", attrs={"class": "column"}):
+        '''
+        for table in soup.find_all("table", class_="column"):
             tr =  table.tr
             self.processBerkeleyData(f, tr)
     
@@ -52,8 +143,13 @@ class BerkeleySpider(Spider):
                 if next_tr.string == None:
                     self.processBerkeleyData(f, next_tr)
     
-    
-    
+        '''
+        for row in parser.rows:
+            url = self.genUrl(row[0: row.find(" ")])
+            print row 
+            self.count = self.count + 1
+            self.write_db(f, row[0:row.find(" ")], row[row.find(" "):], self.getRealUrl(url))
+
         self.close_db(f)
         if file_lines != self.count and self.count > 0:
             self.do_upgrade_db(file_name)
