c54efe5d8e868ced6d01e3883497d1ac02e74b8a
wowdd1 <developergf@gmail.com>
2015-01-24 12:25:48 +0800

    add getCmuFacultyUrl for search cmu faculty

--- find_instructor.py
+++ find_instructor.py
@@ -5,6 +5,7 @@ import getopt
 import sys
 import requests
 from bs4 import BeautifulSoup
+import json
 
 keyword = ''
 
@@ -25,6 +26,23 @@ def getMitFacultyUrl(url):
         if a.text == 'Personal Website':
             return a['href']
     return url
+
+def getCmuFacultyUrl(keyword):
+    post_data = {'combine' : keyword,\
+                 'view_name':"directory_search",\
+                 'view_display_id':"block"}
+    r = requests.post('http://www.cs.cmu.edu/views/ajax', post_data)
+    jobj = r.json()
+    soup = BeautifulSoup(jobj[1]['data'])
+    for a in soup.find_all('a'):
+        if a.attrs.has_key("href") and a['href'].find('www.cs.cmu.edu/directory') != -1:
+            r = requests.get('http://' + a['href'][2 : ])
+            soup = BeautifulSoup(r.text)
+            for a in soup.find_all('a'):
+                if a.text.startswith('http://'):
+                    return a['href']
+                    
+    return ''
 
 def search(keyword):
     for url in faculty_list:
@@ -47,7 +65,10 @@ def search(keyword):
 
             webbrowser.open(url)
             return
-
+    cmu_url = getCmuFacultyUrl(keyword)
+    if cmu_url != '':
+        webbrowser.open(cmu_url)
+        return
     print 'not found'
 
 
