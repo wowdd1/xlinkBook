b7471780e14e995a467e5d9aa6faea2382bb226a
wowdd1 <developergf@gmail.com>
2015-01-28 14:12:23 +0800

    sort the cs course of stanford

--- update/update_stanford_cs.py
+++ update/update_stanford_cs.py
@@ -36,7 +36,7 @@ class StanfordCSSpider(Spider):
             title = title[0 : title.find('(')]
         return title.strip()
  
-    def processStanfordDate(self, f, url):
+    def processStanfordDate(self, f, url, course_dict):
         print 'processing ' + url
         r = requests.get(url)
         soup = BeautifulSoup(r.text)
@@ -45,7 +45,6 @@ class StanfordCSSpider(Spider):
         td_set = []
         td_set_2 = []
         del th_set[0:5]
-    
         i = 0
         for td in td_set_all:
             i = i + 1
@@ -61,7 +60,6 @@ class StanfordCSSpider(Spider):
             link = link[link.find("http"):link.find("EDU") + 3]
             if self.isInCourseNumList(th_set[index].string) == True:
                 continue
-            self.count += 1
             course_id = th_set[index].string.upper()
             description = ''
             description += 'instructors:' + td_set_2[index] + ' '
@@ -69,7 +67,7 @@ class StanfordCSSpider(Spider):
                 description += 'features:Video lectures' + ' '
             if self.description_dict.get(course_id, '') != '':
                 description += 'description:' + self.description_dict[course_id] + ' '
-            self.write_db(f, th_set[index].string.upper(), td_set[index], link, description)
+            course_dict[th_set[index].string.upper()] = CourseRecord(self.get_storage_format(th_set[index].string.upper(), td_set[index], link, description))
 
     def getRecordsDict(self):
         records_dict = {}
@@ -103,8 +101,13 @@ class StanfordCSSpider(Spider):
         self.count = 0
  
         print "processing html and write data to file..."
+        course_dict = {}
         for url in self.getCsCourseLinks():
-            self.processStanfordDate(f, url)
+            self.processStanfordDate(f, url, course_dict)
+
+        for k, record in [(k,course_dict[k]) for k in sorted(course_dict.keys())]:
+            self.count += 1
+            self.write_db(f, k, record.get_title().strip(), record.get_url().strip(), record.get_describe().strip()) 
     
         self.close_db(f)
         if file_lines != self.count and self.count > 0:
