d26727d95f82ec37b6d4fb7ab2ab2ea59e955766
wowdd1 <developergf@gmail.com>
2015-03-04 00:39:43 +0800

    add people info for machine learning and videolectures

--- update/update_videolectures.py
+++ update/update_videolectures.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 from spider import *
+import re
+sys.path.append("..")
+from record import Record
 
 class VideolecturesSpider(Spider):
 
@@ -111,11 +114,70 @@ class VideolecturesSpider(Spider):
         else:
             self.cancel_upgrade(file_name)
             print "no need upgrade\n"
- 
+
+    def upFirstChar(self, text):
+        result = ''
+        for i in range(0, len(text)):
+            if (i > 0 and text[i - 1] == ' ') or i == 0:
+                result += str(text[i]).upper()
+            else:
+                result += text[i]
+
+        return result.strip()
+
+    def processUserData(self):
+        print 'processing user data'
+        file_name = self.get_file_name('eecs/' + self.school + '/user', self.school)
+        file_lines = self.countFileLineNum(file_name)
+        f = self.open_db(file_name + ".tmp")
+        self.count = 0
+        user_dict= {}
+
+        for page in range(1, 24):
+            r = requests.get('http://videolectures.net/site/list/authors/?page=' + str(page))
+            soup = BeautifulSoup(r.text)
+            for tr in soup.find_all('tr'):
+                if tr.text.find('Author') == -1:
+                    soup1 = BeautifulSoup(tr.prettify())
+                    video_pos = tr.text.find('video')
+                    views_pos = tr.text.find('views')
+                    url = 'http://videolectures.net' + soup1.find('a')['href']
+                    desc = ''
+                    vl_id = ''
+                    title = self.upFirstChar(soup1.find('a')['href'][1:].replace('/','').replace('_', ' '))
+                    self.count += 1
+                    if tr.text.find('videos') != -1:
+                        vl_id = str(tr.text[video_pos + 6 : views_pos].strip()) + '-' + str(self.count)
+                    else:
+                        vl_id = str(tr.text[video_pos + 5 : views_pos].strip()) + '-' + str(self.count)
+                    desc = 'description:' + tr.text[views_pos + 5 :]
+
+                    if views_pos == -1:
+                        vl_id = '0' + '-' + str(self.count)
+                        desc = 'description:' + tr.text[video_pos + 5 :]
+                    print vl_id + ' ' + title
+                    user_dict[vl_id] = Record(self.get_storage_format(vl_id, title, url, desc))
+        self.count = 0
+        for item in sorted(user_dict.items(), key=lambda user_dict:int(user_dict[1].get_id()[0 : user_dict[1].get_id().find('-')].strip()), reverse=True):
+            self.count += 1
+            uid = 'vl-' + item[1].get_id()[0 : item[1].get_id().find('-')] + '-' + str(self.count)
+            #print uid + ' ' + item[1].get_title()
+            self.write_db(f, uid, item[1].get_title().strip(), item[1].get_url().strip(), item[1].get_describe().strip())
+            
+                 
+        self.close_db(f)
+        if file_lines != self.count and self.count > 0:
+            self.do_upgrade_db(file_name)
+            print "before lines: " + str(file_lines) + " after update: " + str(self.count) + " \n\n"
+        else:
+            self.cancel_upgrade(file_name)
+            print "no need upgrade\n"
+
     def doWork(self):
         self.processEventData('event')
         for subject in self.subject_cid_map.keys():
             self.processData(subject)
+        self.processUserData()
 
 
 start = VideolecturesSpider()
