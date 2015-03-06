4e2d3272cd78bce4512e92e6831813be5cebeae1
wowdd1 <developergf@gmail.com>
2015-03-06 15:28:24 +0800

    update mas organizations info

--- update/update_mas.py
+++ update/update_mas.py
@@ -71,6 +71,31 @@ class MicrosoftAcademicSearchSpider(Spider):
                         self.cancel_upgrade(file_name)
                         print "no need upgrade\n"                  
 
+    def processOrganizations(self, subject, top_domain_id):
+        print 'processing ' + subject
+        url = self.base_url + '/RankList?entitytype=7&topDomainID=' + top_domain_id + '&subDomainID=0&last=0&start=1&end=100' 
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text)
+        file_name = self.get_file_name(subject + '/' + self.school + '/'+ subject + '-organizations', self.school)
+        file_lines = self.countFileLineNum(file_name)
+        f = self.open_db(file_name + ".tmp")
+        self.count = 0
+
+        for td in soup.find_all('td', class_= 'rank-content'):
+            title = td.a.text
+            url = self.base_url + td.a['href']
+            self.count += 1
+            r_id = 'mas-o-' + str(top_domain_id) + '-0' + '-' + str(self.count)
+            print r_id + ' ' + title
+            self.write_db(f, r_id, title, url) 
+
+        self.close_db(f)
+        if file_lines != self.count and self.count > 0:
+            self.do_upgrade_db(file_name)
+            print "before lines: " + str(file_lines) + " after update: " + str(self.count) + " \n\n"
+        else:
+            self.cancel_upgrade(file_name)
+            print "no need upgrade\n"
 
     def doWork(self):
         r = requests.get(self.base_url)
@@ -78,6 +103,8 @@ class MicrosoftAcademicSearchSpider(Spider):
         for li in soup.find_all('li'):
             if li.a != None and li.a.attrs.has_key("href") and li.a['href'].startswith('/?SearchDomain='):
                 if self.need_update_subject(li.a.text):
+                    arg = li.a['href'].split('&')[0]
+                    self.processOrganizations(li.a.text, arg[arg.find('=') + 1 : ])
                     self.processSubType(li.a.text, self.base_url + li.a['href'])                
 
 
