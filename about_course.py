f70d36be7054fc06f2cc22cf7f3c24fc811c6872
wowdd1 <developergf@gmail.com>
2015-04-08 09:39:00 +0800

    add -u option for about_course.py

--- about_course.py
+++ about_course.py
@@ -10,10 +10,11 @@ def usage():
     print 'usage:'
     print '\t-h,--help: print help message.'
     print '\t-c,--course: the course number'
+    print '-u, --use: seach in what subject'   
 
-def print_course(course_num):
+def print_course(course_num, subject):
     util = Utils()
-    record_list = util.getRecord(course_num, return_all=True)
+    record_list = util.getRecord(course_num, use_subject=subject, return_all=True)
     if record_list == None:
         return
     for record in record_list: 
@@ -21,8 +22,10 @@ def print_course(course_num):
             os.system("./list.py -i " + record.get_path() + " -c 1 -f '^" + course_num + "' -d -r 10")
 
 def main(argv):
+    use_subject = ''
+    course = ''
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hc:', ["help", "course"])
+        opts, args = getopt.getopt(sys.argv[1:], 'hc:u:', ["help", "course", "use"])
         if len(args) == 1:
             print_course(args[0])
     except getopt.GetoptError, err:
@@ -35,7 +38,10 @@ def main(argv):
             usage()
             sys.exit(1)
         elif o in ('-c', '--course'):
-            print_course(a)
+            course = str(a)
+        elif o in ('-u', '--use'):
+            use_subject = str(a)
 
+        print_course(course, use_subject)
 if __name__ == '__main__':
     main(sys.argv)
