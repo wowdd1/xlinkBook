8c2f0d663e9676b3a124f2a7bc30748c4ebe39d8
wowdd1 <developergf@gmail.com>
2015-08-18 23:46:52 +0800

    add bigdatauniversity data

---
+++ update/update_bigdatauniversity.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+
+from spider import *
+
+class BigDataUniversitySpider(Spider):
+    def __init__(self):
+        Spider.__init__(self)
+        self.school = 'BigDataUniversity'
+        self.subject = 'statistics-data-analysis'
+
+
+    def doWork(self):
+        r = requests.get('http://bigdatauniversity.com/wpcourses/')
+        soup = BeautifulSoup(r.text)
+
+
+        file_name = self.get_file_name('statistics-data-analysis/bigdata', self.school)
+        file_lines = self.countFileLineNum(file_name)
+        f = self.open_db(file_name + ".tmp")
+        self.count = 0
+
+
+        for div in soup.find_all('div', class_='media-body'):
+            title = div.h3.a.text
+            instructors = 'instructors:'
+            if div.p != None:
+                instructors += div.p.strong.text
+            self.count += 1
+            self.write_db(f, "bigdata-" + str(self.count), title, div.h3.a['href'], instructors)
+
+        self.close_db(f)
+        if file_lines != self.count and self.count > 0:
+            self.do_upgrade_db(file_name)
+            print "before lines: " + str(file_lines) + " after update: " + str(self.count) + " \n\n"
+        else:
+            self.cancel_upgrade(file_name)
+            print "no need upgrade\n"
+
+start = BigDataUniversitySpider()
+start.doWork()
+
