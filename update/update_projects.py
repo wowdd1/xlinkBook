3b5989fb70f62e37b31fa25f98739081df7006ab
wowdd1 <developergf@gmail.com>
2015-09-13 20:18:39 +0800

    add open source robot projects

--- update/update_projects.py
+++ update/update_projects.py
@@ -12,7 +12,6 @@ class ProjectsSpider(Spider):
         self.utils = Utils()
 
     def doWork(self):
-       '''
        self.getDARPAProjects() 
        self.getDARPAOpenProjects()
        self.getAIProjects()
@@ -21,8 +20,34 @@ class ProjectsSpider(Spider):
        self.getMicrosoftResearch()
        self.getAICProjects()
        self.getDARPAWikiProjects()
-       '''
        self.getSRIProject()
+       self.getOpenSourceRobotProjects()      
+    def getOpenSourceRobotProjects(self):
+        r = requests.get('https://en.wikipedia.org/wiki/Open-source_robotics')
+        soup = BeautifulSoup(r.text)
+        soup = BeautifulSoup(soup.find('table', class_='wikitable').prettify())
+
+        file_name = self.get_file_name("eecs/projects/" + "open-source-robot", self.school)
+        file_lines = self.countFileLineNum(file_name)
+        f = self.open_db(file_name + ".tmp")
+        self.count = 0
+
+        for a in soup.find_all('a'):
+            title = self.utils.removeDoubleSpace(a.text.strip().replace('\n', ''))
+            if title.startswith('[') or title == 'Arduino' or title == 'Self-balancing robot' or title == 'Modular design':
+                continue
+            print title
+            self.count += 1
+            self.write_db(f, "open-source-robot-project-" + str(self.count), title, 'https://en.wikipedia.org' + a['href'])
+
+        self.close_db(f)
+        if file_lines != self.count and self.count > 0:
+            self.do_upgrade_db(file_name)
+            print "before lines: " + str(file_lines) + " after update: " + str(self.count) + " \n\n"
+        else:
+            self.cancel_upgrade(file_name)
+            print "no need upgrade\n"
+           
     def getSRIProject(self):
         r = requests.get("http://www.sri.com/work/projects")
         soup = BeautifulSoup(r.text)
