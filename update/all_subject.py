dae7477645e11afd4ccb3fa87455aac0838f8803
wowdd1 <developergf@gmail.com>
2015-09-16 22:35:35 +0800

    add media-arts-and-sciences course

--- update/all_subject.py
+++ update/all_subject.py
@@ -49,7 +49,7 @@ subject_dict = {\
     "Math" : "mathematics",\
     'Marketing' : 'economics',\
     "Medicine" : "medicine",\
-    "Music" : "music",\
+    "Music" : "art",\
     "Neurobiology" : "neuroscience",\
     "Neuroscience" : "neuroscience",\
     "Philanthropy" : "philosophy",\
@@ -62,16 +62,41 @@ subject_dict = {\
     "Physical" : "physics",\
     'Strategic Management' : 'economics',\
     "Social Sciences" : "social-sciences",\
-    "Science" : "science",\
     "Statistics" : "statistics-data-analysis",\
-    "Data Analysis" : "statistics-data-analysis"}
+    "Data Analysis" : "statistics-data-analysis",\
+    "Civil and Environmental Engineering" : "engineering",\
+    "Mechanical Engineering" : "engineering",\
+    "Materials Science and Engineering" : "engineering",\
+    "Chemical Engineering" : "chemistry",\
+    "Urban Studies and Planning" : "architecture",\
+    "Earth, Atmospheric, and Planetary Sciences" : "energy-earth-sciences",\
+    "Aeronautics and Astronautics" : "engineering",\
+    "Political Science" : "political",\
+    "Biological Engineering" : "engineering",\
+    "Global Studies and Languages" : "education",\
+    "Music and Theater Arts" : "art",\
+    "Nuclear Science and Engineering" : "engineering",\
+    "Media Arts and Sciences" : "others",\
+    "Aerospace Studies" : "others",\
+    "Comparative Media Studies" : "others",\
+    "Computational and Systems Biology" : "others",\
+    "Concourse" : "others",\
+    "Edgerton Center" : "others",\
+    "Engineering Systems" : "engineering",\
+    "Experimental Study Group" : "others",\
+    "Health Sciences and Technology" : "others",\
+    "Military Science" : "others",\
+    "Naval Science" : "others",\
+    "Science, Technology, and Society" : "others",\
+    "Special Programs" : "others",\
+    "Women's and Gender Studies" : "others"}
 
 need_update_subject_list = [
     #'anthropology',
-    'eecs',
-    'mathematics',
+    #'eecs',
+    #'mathematics',
     #'business-management',
-    'economics',
+    #'economics',
     #'rank',
     #'physics',
     #'cognitive-science',
@@ -81,6 +106,7 @@ need_update_subject_list = [
     #'philosophy',
     #"psychology",
     #'chemistry',
+    'others',
     'statistics-data-analysis'
 ]
 
