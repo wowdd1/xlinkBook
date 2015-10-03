dec3f72f3767c69e1bd123572d43bdb21c27f9d6
wowdd1 <developergf@gmail.com>
2015-10-03 21:32:10 +0800

    add mit stanford labs

--- update/update_labs.py
+++ update/update_labs.py
@@ -16,6 +16,100 @@ class LabsSpider(Spider):
         self.getMitMediaLabs()
         self.getSocialRobotLabs()
         self.getBerkeleyEECSLabs()
+        self.getMitLabs()
+        self.getStanfordLabs()
+        self.getStanfordIndependentLabs()
+
+    def getStanfordIndependentLabs(self):
+        r = requests.get('http://doresearch.stanford.edu/node/1200502')
+        soup = BeautifulSoup(r.text)
+        file_name = self.get_file_name(self.school + "/stanford-independent-labs", self.school)
+        file_lines = self.countFileLineNum(file_name)
+        f = self.open_db(file_name + ".tmp")
+        self.count = 0
+
+        for div in soup.find_all('div', class_='tile-title'):
+            print div.text
+            self.count += 1
+            self.write_db(f, "stanford-independent-lab-" + str(self.count), div.text, "")
+
+        self.close_db(f)
+        if file_lines != self.count and self.count > 0:
+            self.do_upgrade_db(file_name)
+            print "before lines: " + str(file_lines) + " after update: " + str(self.count) + " \n\n"
+        else:
+            self.cancel_upgrade(file_name)
+            print "no need upgrade\n"
+
+    def getStanfordLabs(self):
+        r = requests.get('https://www.stanford.edu/research/centers.html')
+        soup = BeautifulSoup(r.text)
+        file_name = self.get_file_name(self.school + "/stanford-labs", self.school)
+        file_lines = self.countFileLineNum(file_name)
+        f = self.open_db(file_name + ".tmp")
+        self.count = 0
+
+        for a in soup.find_all('a', class_='su-link'):
+            print a.text
+            self.count += 1
+            self.write_db(f, "stanford-labs-" + str(self.count), a.text, a['href'])
+            if a.text.startswith('Yacht Research'):
+                break
+
+        self.close_db(f)
+        if file_lines != self.count and self.count > 0:
+            self.do_upgrade_db(file_name)
+            print "before lines: " + str(file_lines) + " after update: " + str(self.count) + " \n\n"
+        else:
+            self.cancel_upgrade(file_name)
+            print "no need upgrade\n"
+
+ 
+    def getMitLabs(self):
+        r = requests.get('http://web.mit.edu/research/')
+        soup = BeautifulSoup(r.text)
+        start = False
+        end = False 
+        for a in soup.find_all('a'):
+            
+            title = a.text.strip()
+            if title.startswith('aeronautics and astronautics'):
+                start = True
+            if start:
+                oneLab = True
+                if a['href'].find('#') != -1:
+                    oneLab = False
+                self.getMitLabsBySubject('http://web.mit.edu/research/' + a['href'], title, oneLab) 
+            if title.startswith('writing'):
+                end = True
+            if end:
+                break
+
+    def getMitLabsBySubject(self, url, subject, oneLab):
+        print subject + " " + url
+        file_name = self.get_file_name(self.school + "/mit-labs/" + subject, self.school)
+        file_lines = self.countFileLineNum(file_name)
+        f = self.open_db(file_name + ".tmp")
+        self.count = 0
+
+        if oneLab:
+            self.count += 1
+            self.write_db(f, "mit-" + subject.replace(' ','-').lower() + "-" + str(self.count), subject, url)
+        else:
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text)
+            for p in soup.find_all('p', class_='TrigListings'):
+                self.count += 1
+                self.write_db(f, "mit-" + subject.replace(' ','-').lower() + "-" + str(self.count), p.a.text, p.a['href'])
+                print p.a.text
+
+        self.close_db(f)
+        if file_lines != self.count and self.count > 0:
+            self.do_upgrade_db(file_name)
+            print "before lines: " + str(file_lines) + " after update: " + str(self.count) + " \n\n"
+        else:
+            self.cancel_upgrade(file_name)
+            print "no need upgrade\n"
 
     def getBerkeleyEECSLabs(self):
         r = requests.get('http://www.eecs.berkeley.edu/Research/Areas/Centers/')
