504ce4511fad7cab2e6eaf6738c5d1596b19ab8d
wowdd1 <developergf@gmail.com>
2015-12-11 15:05:04 +0800

    add all list mergers from wiki

--- update/update_mergers_and_acquisitions.py
+++ update/update_mergers_and_acquisitions.py
@@ -11,7 +11,7 @@ class MergersAndAcquisitionsSpider(Spider):
         self.school = 'wiki'
 
 
-    def doWork(self):
+    def processMergers(self):
         utils = Utils()
         wiki_dict = {'google' : 'http://en.wikipedia.org/wiki/List_of_mergers_and_acquisitions_by_Google',\
                      'facebook' : 'http://en.wikipedia.org/wiki/List_of_mergers_and_acquisitions_by_Facebook',\
@@ -161,5 +161,32 @@ class MergersAndAcquisitionsSpider(Spider):
             else:
                 self.cancel_upgrade(file_name)
                 print "no need upgrade\n"
+    def processAll(self):
+
+        file_name = self.get_file_name('economics/mergers-and-acquisitions/all', self.school)
+        file_lines = self.countFileLineNum(file_name)
+        f = self.open_db(file_name + ".tmp")
+        self.count = 0
+
+        r = requests.get('https://en.wikipedia.org/wiki/Category:Lists_of_corporate_acquisitions')
+        soup = BeautifulSoup(r.text)
+        div = soup.find('div', class_='mw-category')
+        soup = BeautifulSoup(div.prettify())
+        for a in soup.find_all('a'):
+            print a.text.strip()
+            self.count += 1
+            self.write_db(f, 'all-mergers-' + str(self.count), a.text.strip(), 'https://en.wikipedia.org' + a['href'])
+
+        self.close_db(f)
+        if file_lines != self.count and self.count > 0:
+            self.do_upgrade_db(file_name)
+            print "before lines: " + str(file_lines) + " after update: " + str(self.count) + " \n\n"
+        else:
+            self.cancel_upgrade(file_name)
+            print "no need upgrade\n"
+    def doWork(self):
+        self.processAll()
+        self.processMergers()
+        
 start = MergersAndAcquisitionsSpider()
 start.doWork()
