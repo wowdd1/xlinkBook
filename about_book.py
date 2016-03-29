8950678aca45b1753037d23451b3ff0d8d4257ca
wowdd1 <developergf@gmail.com>
2016-03-30 01:04:18 +0800

    suport search amazon in about_book.py

--- about_book.py
+++ about_book.py
@@ -8,12 +8,14 @@ from bs4 import BeautifulSoup;
 from utils import Utils
 
 utils = Utils()
-def searchAmazon(name):
+def searchAmazonCN(name):
     print 'searching ' + name
 
     r = requests.get('http://www.amazon.cn/s/?url=search-alias%3Dstripbooks&field-keywords=' + name)
     soup = BeautifulSoup(r.text)
     div = soup.find('div', class_='a-row a-spacing-small')
+    
+    
     itemid = div.a['href'][div.a['href'].find('dp/') + 3 :]
 
     print 'itemid: ' + itemid
@@ -22,19 +24,30 @@ def searchAmazon(name):
     r = requests.get('http://www.amazon.cn/gp/product-description/ajaxGetProuductDescription.html?asin=' + itemid + '&deviceType=web', headers = user_agent)
 
 
-    soup = BeautifulSoup(r.text)
+    sp = BeautifulSoup(r.text)
     
-    for div in soup.find_all('div'):
-        sp = BeautifulSoup(div.prettify())
+    for div in sp.find_all('div'):
+        sp2 = BeautifulSoup(div.prettify())
         if div.h3 != None:
-            for p in sp.find_all('p'):
+            for p in sp2.find_all('p'):
                 title = p.text.strip()
-                if title == '':
-                    print ''
-                else:
-                    title = title[0:len(title) - 4]
-                    print title
+                print title
+    searchISBNDB(name)
+    print ''
+    print 'other books:'
+    for d in soup.find_all('div', class_='a-row a-spacing-small'):
+        print d.a.text
 
+def searchAmazon(name):
+    print 'searching ' + name
+    user_agent = {'User-agent': 'Mozilla/5.0'}
+    r = requests.get('http://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Dstripbooks&field-keywords=' + name, headers = user_agent)
+    soup = BeautifulSoup(r.text)
+    div = soup.find('div', class_='a-row a-spacing-small')
+    print ' '
+    for d in soup.find_all('div', class_='a-row a-spacing-small'):
+        print d.a.text
+        print ''
 
 def searchISBNDB(name):
     print 'searching ' + name
@@ -68,8 +81,9 @@ def usage(argv0):
 
 def main(argv):
     keyword = ''
+    lang_en = ''
     try:
-        opts, args = getopt.getopt(argv[1:], 'hn:', ["help", "name"])
+        opts, args = getopt.getopt(argv[1:], 'hn:l:', ["help", "name", "lang"])
         if len(args) == 1:
             keyword = args[0]
     except getopt.GetoptError, err:
@@ -81,9 +95,14 @@ def main(argv):
             usage(argv[0])
         if o in ('-n', '--name'):
             keyword = a
+        if o in ('-l', '--lang'):
+            lang_en = a
 
     if keyword != '':
-        searchISBNDB(keyword)
+        if lang_en == 'en':
+            searchAmazon(keyword)
+        else:
+            searchAmazonCN(keyword)
 
 if __name__ == '__main__':
     main(sys.argv)
