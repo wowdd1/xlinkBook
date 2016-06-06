cf860178bf9e5bcb4aeac23721486b105cb887fd
wowdd1 <developergf@gmail.com>
2016-06-06 19:03:25 +0800

    remove unuseful code

--- utils.py
+++ utils.py
@@ -11,7 +11,6 @@ import itertools
 import unicodedata
 from update.all_subject import default_subject
 import prettytable
-from prettytable import PrettyTable
 import requests
 import sys
 from bs4 import BeautifulSoup;
@@ -33,81 +32,9 @@ if py3k:
     basestring = str
     itermap = map
     uni_chr = chr
-    from html.parser import HTMLParser
 else:
     itermap = itertools.imap
     uni_chr = unichr
-    from HTMLParser import HTMLParser
-
-class TableHandler(HTMLParser):
-
-    def __init__(self, **kwargs):
-        HTMLParser.__init__(self)
-        self.kwargs = kwargs
-        self.tables = []
-        self.last_row = []
-        self.rows = []
-        self.max_row_width = 0
-        self.max_cell_width = 100
-        self.active = None
-        self.last_content = ""
-        self.is_last_row_header = False
-
-    def setMaxCellWidth(self, width):
-        self.max_cell_width = width
-
-    def handle_starttag(self,tag, attrs):
-        self.active = tag
-        if tag == "th":
-            self.is_last_row_header = True
-
-    def handle_endtag(self,tag):
-        if tag in ["th", "td"]:
-            stripped_content = self.last_content.strip()[0 : self.max_cell_width]
-            self.last_row.append(stripped_content)
-        if tag == "tr":
-            self.rows.append(
-                (self.last_row, self.is_last_row_header))
-            self.max_row_width = max(self.max_row_width, len(self.last_row))
-            self.last_row = []
-            self.is_last_row_header = False
-        if tag == "table":
-            table = self.generate_table(self.rows)
-            self.tables.append(table)
-            self.rows = []
-        if tag == "span":
-            return
-        self.last_content = " "
-        self.active = None
-    def handle_data(self, data):
-        self.last_content += data
-    def generate_table(self, rows):
-        """
-        Generates from a list of rows a PrettyTable object.
-        """
-        table = PrettyTable(**self.kwargs)
-        for row in self.rows:
-            if len(row[0]) < self.max_row_width:
-                appends = self.max_row_width - len(row[0])
-                for i in range(1,appends):
-                    row[0].append("-")
-
-            if row[1] == True:
-                self.make_fields_unique(row[0])
-                table.field_names = row[0]
-            else:
-                table.add_row(row[0])
-        return table
-
-    def make_fields_unique(self, fields):
-        """
-        iterates over the row and make each field unique
-        """
-        for i in range(0, len(fields)):
-            for j in range(i+1, len(fields)):
-                if fields[i] == fields[j]:
-                    fields[j] += "'"
-
 
 class Utils:    
     cc_map = {
