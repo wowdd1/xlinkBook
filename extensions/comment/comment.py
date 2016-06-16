0abb45824b9e0fe1f79a7a8d8d3ff54d92b09f90
wowdd1 <developergf@gmail.com>
2016-06-16 15:01:13 +0800

    add comment for arxiv paper

---
+++ extensions/comment/comment.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from extensions.bas_extension import BaseExtension
+
+class Comment(BaseExtension):
+
+    def excute(self, form_dict):
+        rID = form_dict['rID'].encode('utf8')
+
+        html = ''
+        rID = rID[rID.find('-') + 1 : ].replace('-', '.')
+        url = "https://scirate.com/arxiv/" + rID 
+        html += '<br/><iframe src="' + url + '" style="border: 0; width: 100%; height: 400px"></iframe><br/>'
+
+        url = 'http://www.shortscience.org/paper?bibtexKey=journals/corr/' + rID
+        html += '<br/><iframe src="' + url + '" style="border: 0; width: 100%; height: 400px"></iframe><br/>'
+   
+        #url = 'http://gitxiv.com/search/?q=' + form_dict['rTitle']
+        #html += '<br/><iframe src="' + url + '" style="border: 0; width: 100%; height: 400px"></iframe><br/>'
+        return html
+
+    def check(self, form_dict):
+        rID = form_dict['rID'].encode('utf8')
+        return rID.find('arxiv') != -1
