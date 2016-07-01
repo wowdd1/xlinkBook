f30c67a4a234e8a2a3991f4557c0492efefe863f
wowdd1 <developergf@gmail.com>
2016-07-01 10:56:06 +0800

    update cvpr 2016 data

--- update/update_ai_papers.py
+++ update/update_ai_papers.py
@@ -4,6 +4,7 @@
 from spider import *
 sys.path.append("..")
 from utils import Utils
+from record import Record
 
 class AiPapersSpider(Spider):
     def __init__(self):
@@ -357,45 +358,81 @@ class AiPapersSpider(Spider):
             self.cancel_upgrade(file_name)
             print "no need upgrade\n"
 
-    def getCVPR2015Paper(self):
-        r = requests.get('http://www.cv-foundation.org/openaccess/CVPR2015.py')
+    def getCVFoundationPapers(self, url):
+        r = requests.get(url)
         soup = BeautifulSoup(r.text)
         title_list = []
         author_list = []
         link_list = []
-
-        file_name = self.get_file_name("eecs/papers/" + self.school.lower() + '/' + 'cvpr-2015', "scholaroctopus")
-        file_lines = self.countFileLineNum(file_name)
-        f = self.open_db(file_name + ".tmp")
-        self.count = 0
+        records = []
 
         for dt in soup.find_all('dt', class_='ptitle'):
             title_list.append(dt.text.strip())
         count = 0
         for dd in soup.find_all('dd'):
             if count % 2 == 0:
-                author_list.append(self.util.removeDoubleSpace(dd.text.strip().replace('\n', '')))
+                author_list.append(self.util.removeDoubleSpace(dd.text.strip().replace('\n', '')).replace(',', ', '))
             else:
-                link_list.append("http://www.cv-foundation.org/openaccess/" + dd.a['href'])
+                if dd.a['href'].find('openaccess') == -1:
+                    link_list.append("http://www.cv-foundation.org/openaccess/" + dd.a['href'])
+                else:
+                    link_list.append("http://www.cv-foundation.org" + dd.a['href'])
             count += 1
         for i in range(0, len(title_list)):
-            print i
-            print title_list[i]
-            print author_list[i]
-            print link_list[i]
-            self.count += 1
-            self.write_db(f, 'cvpr-2015-' + str(self.count), title_list[i], link_list[i], "author:" + author_list[i])
+            #print i
+            #print title_list[i]
+            #print author_list[i]
+            #print link_list[i]
+            records.append(Record(' | ' + title_list[i] + ' | ' + link_list[i] + ' | ' + "author:" + author_list[i]))
+        return records
+
+    def saveCVFoundationPapers(self, records, year, conf):
+
+        file_name = self.get_file_name("eecs/papers/" + self.school.lower() + '/' + conf + '-' + year, "scholaroctopus")
+        file_lines = self.countFileLineNum(file_name)
+        f = self.open_db(file_name + ".tmp")
+        self.count = 0
+        for record_list in records:
+            for record in record_list:
+                self.count += 1
+                print record.get_title().strip()
+                self.write_db(f, conf + '-' + year + '-' + str(self.count), record.get_title().strip(), record.get_url().strip(), record.get_describe().strip())
         self.close_db(f)
         if file_lines != self.count and self.count > 0:
             self.do_upgrade_db(file_name)
             print "before lines: " + str(file_lines) + " after update: " + str(self.count) + " \n\n"
         else:
             self.cancel_upgrade(file_name)
-            print "no need upgrade\n"            
+            print "no need upgrade\n"
+
+    def getCVFoundation(self):
+        r = requests.get('http://www.cv-foundation.org/openaccess/menu.py')
+        soup = BeautifulSoup(r.text)
+        for dd in soup.find_all('dd'):
+            title = dd.text[0: dd.text.find(',')].strip()
+            print title
+            year = title[title.find(' ') :].strip()
+            conf = title[0 : title.find(' ')].strip().lower()
+            
+            records = []
+            sp = BeautifulSoup(dd.prettify())
+            for a in sp.find_all('a'):
+                if a.text.strip().startswith('Main'):
+                    records.append(self.getCVFoundationPapers(a['href']))
+                    print 'Main'
+                else:
+                    r2 = requests.get(a['href'])
+                    sp2 = BeautifulSoup(r2.text)
+                    for a in sp2.find_all('a'):
+                        if a['href'].endswith('.py') and a.text != 'Back':
+                            print a.text
+                            records.append(self.getCVFoundationPapers(a['href']))
+
+            self.saveCVFoundationPapers(records, year, conf)
+                
 
 start = AiPapersSpider()
 
-#start.getCVPR2015Paper()
 start.doWork()
 start.getCvPaper()
 start.getNipsPaper()
@@ -404,6 +441,7 @@ start.getNlpPaper()
 start.getJMLRPaper()
 start.getRSSPaper()
 start.getIJCAIPaper()
+start.getCVFoundation()
 
 #TODO
 #TPAMI http://dblp.uni-trier.de/db/journals/pami/
