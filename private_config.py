93d91bfbcc290d0fbf52bcfde90288e717b241e7
wowdd1 <developergf@gmail.com>
2019-03-27 10:33:09 +0800

    add private config

---
+++ private_config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8-*- 
+
+from config import Config
+class PrivateConfig():
+
+    searchLibraryTitleDict = {}
+
+    searchLibraryDescFilterDict = {}
+    
+    convert_dict = {}
