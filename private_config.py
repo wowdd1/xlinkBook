63bf4c91046a1af47da2c4eb86bac200a167312e
wowdd1 <developergf@gmail.com>
2019-10-08 16:07:03 +0800

    update

---
+++ private_config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8-*- 
+
+from config import Config
+class PrivateConfig():
+
+
+    processSourceDict = {}
+
+    
+    processSearchCommandDict = {}
+    
+
+    processPostCommandDict = {}
+    # url keyword + args
+    # resource type + args
+
+    convert_command_dict = {}
+
+    convert_source2engine_dict = {}
+
+    convert_dict = {}
+
+
+    innerSearchDict = {}
+
+    groupSearchDict = {}
