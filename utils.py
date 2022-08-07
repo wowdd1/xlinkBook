125d891f3f702bf30e5da82a297d533df47c281b
wowdd1 <developergf@gmail.com>
2022-08-07 08:57:22 +0800

    fix

--- utils.py
+++ utils.py
@@ -4627,7 +4627,9 @@ class Utils:
                 subSearchin = self.loadSubSearchin(">" + field, parentOfSearchin, 446)
                 if subSearchin != "":
                     result += subSearchin 
-                    if len(cmds) > 12:
+                    if len(cmds) > 18:
+                        result += "<br><br><br><br><br><br><br><br>"
+                    elif len(cmds) > 12:
                         result += "<br><br><br><br><br><br>"
                     elif len(cmds) > 6:
                         result += "<br><br><br><br>"
