62310c93f2895e85d63cf41393ac846b8da16e71
wowdd1 <developergf@gmail.com>
2022-09-21 17:41:03 +0800

    update

--- utils.py
+++ utils.py
@@ -4613,7 +4613,7 @@ class Utils:
                             html += self.genPreviewLink(newAID, itemText, self.getRepoImage(repo))
                             html += self.genIconLinkHtml("https://ossinsight.io/analyze/" + repo, Config.website_icons['analyze'])
                             html += self.genIconLinkHtml("https://useful-forks.github.io/?repo=" + repo + "*" + "https://techgaun.github.io/active-forks/#" + repo, Config.website_icons['fork'])
-                            html += self.genIconLinkHtml("https://releaseeye.info/" + repo, Config.website_icons['release'])
+                            html += self.genIconLinkHtml("https://github.com/" + repo +  "/commits" + "*" + "https://releaseeye.info/" + repo, Config.website_icons['release'])
                             html += self.genIconLinkHtml("https://github1s.com/" + repo + "*" + "https://replit.com/github/" + repo, Config.website_icons['ide'])
                             html += self.genIconLinkHtml("https://gitplanet.com/" + repo + "*" + "https://www.yasiv.com/github/#/costars?q=" + repo, Config.website_icons['repos'])
                         else:    
@@ -4635,7 +4635,7 @@ class Utils:
                             html += self.genPreviewLink(newAID, item, self.getRepoImage(repo))
                             html += self.genIconLinkHtml("https://ossinsight.io/analyze/" + repo, Config.website_icons['analyze'])
                             html += self.genIconLinkHtml("https://useful-forks.github.io/?repo=" + repo + "*" + "https://techgaun.github.io/active-forks/#" + repo, Config.website_icons['fork'])
-                            html += self.genIconLinkHtml("https://releaseeye.info/" + repo, Config.website_icons['release'])
+                            html += self.genIconLinkHtml("https://github.com/" + repo +  "/commits" + "*" + "https://releaseeye.info/" + repo, Config.website_icons['release'])
                             html += self.genIconLinkHtml("https://github1s.com/" + repo + "*" + "https://replit.com/github/" + repo, Config.website_icons['ide'])
                             html += self.genIconLinkHtml("https://gitplanet.com/" + repo + "*" + "https://www.yasiv.com/github/#/costars?q=" + repo, Config.website_icons['repos'])
                         else:
