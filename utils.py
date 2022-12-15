732790011bace58c1345582fe85d7a75e7c33f88
wowdd1 <developergf@gmail.com>
2022-12-15 18:46:10 +0800

    fix

--- utils.py
+++ utils.py
@@ -5292,7 +5292,13 @@ class Utils:
                 if parentDivID != '':
                     js = "typeKeywordEx('" + self.decodeCommand(value) + "', '" + parentOfSearchin + "', false, '" + parentDivID + "');chanageLinkColor(this, '#E9967A', '');"
 
-                result += '<a href="javascript:void(0);" onclick="' + js + '" onmouseover="' + js2 + '" style="color: rgb(153, 153, 102); font-size:9pt;">' + text + '</a>, '
+                result += '<a href="javascript:void(0);" onclick="' + js + '" onmouseover="' + js2 + '" style="color: rgb(153, 153, 102); font-size:9pt;">' + text + '</a>'
+                if parentDivID.find("combine-result") != -1:
+                    js = "showPopupContent(pageX, pageY, 550, 480, '#>" + text + "/:');"
+                    result += ' <a href="javascript:void(0);" onclick="' + js + '" >' + self.getIconHtml('', 'tabs', width=10, height=8) + '</a>'
+                    result += ", "
+                else:
+                    result += ", "
 
              
             if result.endswith(', '):
