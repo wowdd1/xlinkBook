d88db5176aaecd081e421548c843f244f534006d
wowdd1 <developergf@gmail.com>
2023-04-05 18:01:27 +0800

    add papers with code

--- config.py
+++ config.py
@@ -466,6 +466,7 @@ class Config():
                  'udacity' : 'https://www.uplabs.com/assets/integrations/udacity-92b3b2525603489c7c5f325491d0ff44652631210086bb2ab082b897b9b39da0.png',\
                  'github' : 'https://cdn2.iconfinder.com/data/icons/black-white-social-media/64/social_media_logo_github-128.png',\
                  'huggingface' : 'https://huggingface.co/front/assets/huggingface_logo-noborder.svg',\
+                 'paperswithcode' : 'https://paperswithcode.com/static/logo.png',\
                  'civitai' : 'https://cdn-1.webcatalog.io/catalog/civitai/civitai-icon-filled-256.png?v=1676420128023',\
                  'replicate' : 'https://replicate.com/static/apple-touch-icon.bacafbed483e.png',\
                  'modelscope' : 'https://img.alicdn.com/imgextra/i4/O1CN01fvt4it25rEZU4Gjso_!!6000000007579-2-tps-128-128.png',\
